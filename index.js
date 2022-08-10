687bc930ed8dc36deb1e2c426f2956823eff8516
aspencass <caspenw1@gmail.com>
2022-08-10 11:04:52 -0600

    commit tree 5bcd128e26e373da19a0501a09ce45bf0d87929b

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+
+
+const dodger = document.getElementById("dodger")
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+
+  const width = 360
+
+
+function moveDodgerRight() {
+    const RightNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(RightNumbers, 10);
+  
+
+    if (left <= width) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+
